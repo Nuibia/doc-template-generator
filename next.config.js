68a68801963f0e26443560d2df67605400150858
anhf <anhf@xiaoma.cn>
2025-03-21 10:27:08 +0800

    chore:  更新配置：将构建输出目录设置为'dist'，并添加对图像的非优化处理

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const withLess = require('next-with-less');
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['antd', '@ant-design', 'rc-util', 'rc-pagination', 'rc-picker'],
+  distDir: 'dist',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = withLess({
